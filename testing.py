16723868d0aef64ce5a4fcec9b83b22123edfb93
Ollie Lloyd <ollielloyd96@outlook.com>
2019-05-24 12:42:26 +0100

    removed testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-# Get dictionary of live streams
-from twitch import TwitchClient
-
-client = TwitchClient(client_id='2ieanu14o1w8iphbl3smon6fbik2y5')
-live = client.streams.get_live_streams()
-streams = {}
-for i in live:
-    streams[i['channel']['display_name']] = i
-  
