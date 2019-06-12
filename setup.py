fa29c9e6fc72f3423d49e0f2af8f02727a2697b5
Ollie Lloyd <ollielloyd96@outlook.com>
2019-06-12 13:27:31 +0100

    removed logging from requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'twitch-listener',         # How you named your package folder (MyLib)
-  packages = ['twitch-listener'],   # Chose the same as "name"
+  name = 'twitch_listener',         # How you named your package folder (MyLib)
+  packages = ['twitch_listener'],   # Chose the same as "name"
   version = '1.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tools to scrape Twitch chat data',   # Give a short description about your library
   author = 'Oliver Lloyd',                   # Type in your name
   author_email = 'ollielloyd96@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/lloyd334/twitch-listener',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['Twitch', 'chatbot', 'scraper'],   # Keywords that define your package best
+  download_url = 'https://github.com/lloyd334/twitch-listener/archive/1.0.0.tar.gz',    # I explain this later on
+  keywords = ['Twitch', 'twitch.tv', 'chatbot', 'chat', 'bot', 'scraper', 'scraping'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pandas',
+          'requests'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
@@ -20,9 +20,11 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   
 
-    'Programming Language :: Python :: 3'      #Specify which pyhton versions that you want to support
-    
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+'Programming Language :: Python :: 3.6',
   ],
 )
