c96ba91bb6c547e5ef8aae21f920dbdd03e64949
Ollie Lloyd <ollielloyd96@outlook.com>
2019-06-17 10:37:07 +0100

    updated

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
           'requests'
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
