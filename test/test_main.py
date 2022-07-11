d7536c20513d96432490573235e2fe658b1d30d6
macros.mene <macros.mene@gmail.com>
2022-07-11 17:02:10 +0200

    setup infrastructure of test environment for the new class TwitchConnector

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,5 @@
 import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+from test_twitchIRCConnector import TestTwitchConnector
 
 
 if __name__ == '__main__':
---
+++ test/test_twitchIRCConnector.py
@@ -0,0 +1,11 @@
+import unittest
+from twitch_chat_recorder.twitchIRCConnector import TwitchConnector
+
+
+class TestTwitchConnector(unittest.TestCase):
+
+    def test_init(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_create_channel_connections(self):
+        self.assertEqual(True, True)  # add assertion here
