c97356dc6ac3e0872fc26fae9175b7f42f06f9ce
macros.mene <macros.mene@gmail.com>
2022-07-14 16:08:41 +0200

    IRC Connector fix and tested

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,5 @@
 import unittest
 from test_twitchIRCConnector import TestTwitchConnector, TestTwitchChatterbox
-from test_twitchIRCStreamReader import TestTwitchIRCStreamReader
 
 if __name__ == '__main__':
     unittest.main()
