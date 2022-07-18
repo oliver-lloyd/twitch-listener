3820dc3007af46208fb6a7947d816d7011bd10c6
macros.mene <macros.mene@gmail.com>
2022-07-18 20:26:13 +0200

    UPDATE README file with a significant piece of code as example

--- twitch_chat_recorder/__init__.py
+++ twitch_chat_recorder/__init__.py
@@ -1,8 +1,5 @@
-import sys
-sys.path.append('../../')
 from twitch_chat_recorder.twitchIRCConnector import TwitchConnector, TwitchChatterbox
 from twitch_chat_recorder.twitchIRCController import TwitchIRCController
 from twitch_chat_recorder.twitchIRCStreamReader import TwitchIRCStreamReader
 from twitch_chat_recorder.twitchStoredInfo import StreamInfo
 
-from twitch_chat_recorder import utils
