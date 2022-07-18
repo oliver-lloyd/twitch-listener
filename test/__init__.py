71fa248fb0beefd0a835e684fc11f15194b9b456
macros.mene <macros.mene@gmail.com>
2022-07-18 20:31:47 +0200

    UPDATE README file with a significant piece of code as example

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,6 @@
 from test.test_twitchIRCConnector import TestTwitchConnector, TestTwitchChatterbox
 from test.test_twitchIRCStreamReader import TestTwitchIRCStreamReader
-from ..twitch_chat_recorder.twitchIRCConnector import TwitchConnector, TwitchChatterbox
-from ..twitch_chat_recorder.twitchIRCController import TwitchIRCController
-from ..twitch_chat_recorder.twitchIRCStreamReader import TwitchIRCStreamReader
-from ..twitch_chat_recorder.twitchStoredInfo import StreamInfo
+from twitch_chat_recorder.twitchIRCConnector import TwitchConnector, TwitchChatterbox
+from twitch_chat_recorder.twitchIRCController import TwitchIRCController
+from twitch_chat_recorder.twitchIRCStreamReader import TwitchIRCStreamReader
+from twitch_chat_recorder.twitchStoredInfo import StreamInfo
