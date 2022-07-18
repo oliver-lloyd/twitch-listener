8a4651ddab451101dd56341d9697275c0f734242
macros.mene <macros.mene@gmail.com>
2022-07-18 19:16:30 +0200

    UPDATE README file with a significant piece of code as example

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,7 @@
 from test_main import *
 from test_twitchIRCConnector import *
 from test_twitchIRCStreamReader import *
+from twitch_chat_recorder.twitchIRCConnector import *
+from twitch_chat_recorder.twitchIRCController import *
+from twitch_chat_recorder.twitchIRCStreamReader import *
+from twitch_chat_recorder.twitchStoredInfo import *
